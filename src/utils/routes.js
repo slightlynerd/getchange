0d6c96863e31bdc002e55955aa01f94ca60c69e8
Kelvin Gobo <gobokel@gmail.com>
2020-08-10 05:33:03 +0100

    update api url

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://crudcrud.com/api/421b2755d9004362a916247c16c34243'
+export const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://crudcrud.com/api/44bb61d4a03645c7823058befb3486b7'
 
 export const EMPLOYEES = '/employees'
